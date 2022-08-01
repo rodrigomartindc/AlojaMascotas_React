e01dcb99a49128257aa4643098d858b6df5a521e
rodrigomartindc <rodrigomartindc@gmail.com>
2022-08-01 20:36:08 -0300

    update img src

--- alojamascotas-react/src/components/PanelProductsList.js
+++ alojamascotas-react/src/components/PanelProductsList.js
@@ -15,7 +15,7 @@ function PanelProductsList(props) {
           <div className="text-center">
             <img
               className="img-fluid px-3 px-sm-4 mt-3 mb-4"
-              src= {props.image}
+              src= {"http://localhost:8000/img/guarderias/" + props.image}
               alt={props.name}
               style={{ width: "90%", height: "400px", objectFit: "cover" }}
             />
